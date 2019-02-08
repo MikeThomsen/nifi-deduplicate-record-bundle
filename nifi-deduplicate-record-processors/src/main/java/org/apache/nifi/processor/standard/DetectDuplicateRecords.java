c66bb0e9040bc8419732abfd16a825883c738b07
Mike <mikerthomsen@gmail.com>
2019-02-08 18:40:12 -0500

    Added relationships.

--- nifi-deduplicate-record-processors/src/main/java/org/apache/nifi/processor/standard/DetectDuplicateRecords.java
+++ nifi-deduplicate-record-processors/src/main/java/org/apache/nifi/processor/standard/DetectDuplicateRecords.java
@@ -11,6 +11,7 @@ import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.processor.AbstractProcessor;
 import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.util.StandardValidators;
 import org.apache.nifi.record.path.util.RecordPathCache;
@@ -19,7 +20,9 @@ import org.apache.nifi.serialization.RecordSetWriterFactory;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @InputRequirement(InputRequirement.Requirement.INPUT_REQUIRED)
 @Tags({"record", "duplicate", "map", "cache", "detect"})
@@ -60,6 +63,32 @@ public class DetectDuplicateRecords extends AbstractProcessor {
     public static final List<PropertyDescriptor> DESCRIPTORS = Collections.unmodifiableList(Arrays.asList(
         RECORD_READER, RECORD_WRITER, MAP_CACHE_SERVICE, RECORD_PATH
     ));
+
+    public static final Relationship REL_DUPLICATES = new Relationship.Builder()
+        .name("duplicates")
+        .description("Duplicates are assembled into a record set for this relationship.")
+        .build();
+    public static final Relationship REL_NOT_DUPLICATE = new Relationship.Builder()
+        .name("not duplicates")
+        .description("Records that have not been detected as duplicates are assembled into a record set for this relationship.")
+        .build();
+    public static final Relationship REL_ORIGINAL = new Relationship.Builder()
+        .name("original")
+        .description("The original input flowfile is sent to this relationship unless there is a fatal error in the processing.")
+        .build();
+    public static final Relationship REL_FAILURE = new Relationship.Builder()
+        .name("failure")
+        .description("When processing fails, the input flowfile goes to this relationship.")
+        .build();
+
+    public static final Set<Relationship> RELATIONSHIPS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
+        REL_DUPLICATES, REL_NOT_DUPLICATE, REL_ORIGINAL, REL_FAILURE
+    )));
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return RELATIONSHIPS;
+    }
 
     @Override
     public List<PropertyDescriptor> getSupportedPropertyDescriptors() {
